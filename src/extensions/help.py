e0095457c734bb759905ba574a125cb54b9d03bc
kian85 <kian.shahrami.1385@gmail.com>
2024-07-28 17:33:09 +0330

    Fixed formatting for help.py

--- src/extensions/help.py
+++ src/extensions/help.py
@@ -1,17 +1,23 @@
 import disnake
 from disnake.ext import commands
 import typing as t
+
 if t.TYPE_CHECKING:
     from src.bot import Bot
+
 
 class HelpCog(commands.Cog):
     def __init__(self, bot: "Bot"):
         self.bot = bot
 
-    @commands.slash_command(name="help", description="Shows a help message describing commands")
+    @commands.slash_command(
+        name="help", description="Shows a help message describing commands"
+    )
     async def help(self, inter: disnake.ApplicationCommandInteraction):
         """Sends a help message with all available commands."""
-        embed = disnake.Embed(title="Help", description="List of all commands", color=0x00ff00)
+        embed = disnake.Embed(
+            title="Help", description="List of all commands", color=0x00FF00
+        )
 
         for command in self.bot.slash_commands:
             embed.add_field(
@@ -21,6 +27,7 @@ class HelpCog(commands.Cog):
             )
 
         await inter.response.send_message(embed=embed, ephemeral=True)
+
 
 def setup(bot: "Bot"):
     bot.add_cog(HelpCog(bot))
