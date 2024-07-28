855c162b259d1dcf18d786dd64a80147a9f558b4
aryan <85390033+theredditbandit@users.noreply.github.com>
2024-07-28 21:28:07 +0530

    reimplement ask command

--- src/extensions/ask.py
+++ src/extensions/ask.py
@@ -1,80 +1,51 @@
-import disnake
 import groq
+import disnake
 import typing as t
-import nltk
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from collections import Counter
 from disnake.ext import commands
-from src.utils.base.embed import createEmbed, sendEmbed
 from src.utils.base.ext import Extension
-from src.utils.env import ENV, CONFIG
+from src.utils.base.embed import createEmbed
 from src.utils.messages import Messages
+from src.utils.prompts import get_ask_prompt, ASK_SYSTEM_PROMPT
+from src.utils.env import ENV, CONFIG
 from src.utils.logger import Logger
 
 if t.TYPE_CHECKING:
     from src.bot import Bot
 
-nltk.download("punkt")
-nltk.download("stopwords")
-
 
 class Ask(Extension):
     def __init__(self, bot: "Bot"):
         self.client = groq.Client(api_key=ENV.groq)
-        self.msgHandler = Messages()
-        self.logger = Logger("Summarize", fileLogging=CONFIG.env == "prod")
+        self.logger = Logger("Ask", fileLogging=CONFIG.env == "prod")
 
     @commands.slash_command(
         name="ask",
-        description="ask a query that returns the highest rated document in a channel/thread",
+        description="Answers a query based on the information present in the channel/thread",
     )
-    async def summary(self, inter: disnake.ApplicationCommandInteraction, query: str):
+    async def ask(self, inter: disnake.ApplicationCommandInteraction, query: str):
+        await inter.response.defer(ephemeral=True)
         thread_name = inter.channel
-        msg = await self.msgHandler.getMessages(
-            channel=thread_name, limit=100, order="asc"
+        msg = await Messages.getMessages(channel=thread_name, limit=4000, order="asc")
+        context = "\n".join([f"{i.author} says {i.content}" for i in msg])
+        completion = self.client.chat.completions.create(
+            model="llama-3.1-70b-versatile",
+            messages=[
+                {"role": "system", "content": ASK_SYSTEM_PROMPT},
+                {"role": "user", "content": get_ask_prompt(context, query)},
+            ],
+            temperature=1,
+            max_tokens=1024,
+            top_p=1,
         )
-        corpus = []
-        for message in msg:
-            corpus.append(message.content)
-        corpus = [i for i in corpus if i]
-        self.logger.info(f"messages indexed into corpus: {corpus}")
-        retrieved_docs = self.retrieve_documents(query, corpus)
-        response = self.generate_response(retrieved_docs)
+
+        sol = completion.choices[0].message.content
+
         embed = createEmbed(
-            inter.user, title=f'{thread_name} query for "{query}"', description=response
+            inter.user, title=f"`Answering Query {query}`", description=sol
         )
-        await sendEmbed(inter, embed, first=True)
 
-    def retrieve_documents(self, query, corpus):
-        stop_words = set(stopwords.words("english"))
-        query_tokens = [
-            word.lower()
-            for word in word_tokenize(query)
-            if word.isalnum() and word.lower() not in stop_words
-        ]
-        query_counter = Counter(query_tokens)
-        scores = []
-        for doc in corpus:
-            doc_tokens = [
-                word.lower()
-                for word in word_tokenize(doc)
-                if word.isalnum() and word.lower() not in stop_words
-            ]
-            doc_counter = Counter(doc_tokens)
-            score = sum(
-                query_counter[word] * doc_counter[word] for word in query_counter
-            )
-            scores.append((score, doc))
-        scores.sort(reverse=True, key=lambda x: x[0])
-        return scores
-
-    def generate_response(self, retrieved_docs):
-        if not retrieved_docs or retrieved_docs[0][0] == 0:
-            return "Couldn't find any information related to your query."
-        top_doc = retrieved_docs[0][1]
-        response_template = "Here's some information, based on your query: {}"
-        return response_template.format(top_doc)
+        # await sendEmbed(inter, embed, first=False)
+        await inter.followup.send(embed=embed)
 
 
 def setup(bot: "Bot"):
